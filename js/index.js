b7c15ea0c5abdb905b323fda079d9679993c0850
liyiwen <2294037542@qq.com>
2018-04-13 19:42:28 +0800

    360

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/*
+* @Author: Administrator
+* @Date:   2018-03-21 10:27:56
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-03-21 20:52:51
+*/
+// var i=0;
+  $(function(){
+	
+// 		$(".swiper-button-next").click(function(){
+// 			i++;
+// 		if(i==3){
+// 			i=0;
+// 		}
+// 		$(".swiper-slide").eq(i).fadeIn(200).siblings().fadeOut(200);
+// 	});
+ 	
+ 	});
+ 	
